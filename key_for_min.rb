c397ff19bee005056f54c61df062a890bce0ca1b
Anthony Lepore <violacellonyc@gmail.com>
2020-07-31 13:27:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,16 @@ sizes = []
     sizes.push(j)
   end
   
-  
+# find smallest value in Array of sizes 
+
+small_size = 100000 * 100000
+
+  sizes.each do |i|
+    if i < small_size then small_size = i 
+  end
+ end
+ 
+ 
 
 
 
